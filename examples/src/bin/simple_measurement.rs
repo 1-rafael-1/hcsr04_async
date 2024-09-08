d3a41acc2c39bf888597a9fdf28cf9b861059215
rafael <rafael.koch@gmx.net>
2024-09-08 09:24:44 +0200

    v0.1.1, add average filter example, fix name in readme

--- examples/src/bin/simple_measurement.rs
+++ examples/src/bin/simple_measurement.rs
@@ -1,3 +1,14 @@
+//! # Simple Measurement
+//! This example demonstrates how to use the HCSR04 sensor to measure distance in a loop.
+//!
+//! ## Note
+//!
+//! Most of the hc-sr04 sensors are rated for 5V. The Raspberry Pi Pico is a 3.3V device. While some hc-sr04 tolerate to be operated on 3.3V, it is not recommended to do so. At best the sensor will have a reduced precision.
+//! When operating the sensor at 5V with a 3.3V controller like the Pi Pico, the following considerations must be taken into account:
+//!
+//! - The trigger pin can be directly connected to the controller. 3.3V should be enough to trigger the sensor.
+//! - The echo pin must be connected to the controller through a voltage divider to reduce the voltage from 5V to 3.3V. The echo pin on the hc-sr04 will output 5V when the sensor is operated at 5V and that will then damage the controller.
+
 #![no_std]
 #![no_main]
 
